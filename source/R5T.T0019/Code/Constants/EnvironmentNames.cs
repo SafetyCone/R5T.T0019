9f7525bb6f200ec26596029c330a5f9b950ad3f5
David Coats <david@getsafetycone.com>
2021-05-21 17:46:26 -0700

    Add unsaved details.

---
+++ source/R5T.T0019/Code/Constants/EnvironmentNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace R5T.T0019
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This also exists in Microsoft.Extensions.Hosting.Abstractions, Microsoft.Extensions.Hosting.Environments.
+    /// </remarks>
+    public static class EnvironmentNames
+    {
+        public const string Development = "Development";
+        public const string Production = "Production";
+        public const string Statging = "Staging";
+    }
+}
